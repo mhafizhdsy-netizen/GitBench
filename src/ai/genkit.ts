5a01bbb899b747110d176f4752cd7a97eb190a63
MuhammadHafizh <mhafizhdsy@gmail.com>
2025-11-30 02:45:01 +0000

    perbaiki ai tidak bisa generate commit walaupun sudah memakai apikey gem

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,12 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [
+    googleAI({
+      models: {
+        'gemini-2.5-flash': {model: 'gemini-2.5-flash-preview-0514'},
+      },
+    }),
+  ],
   model: 'googleai/gemini-2.5-flash',
 });
