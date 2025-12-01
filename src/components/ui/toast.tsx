b43ea74a9af2d95f6cf7f4d73b0361688b26f8a3
MuhammadHafizh <mhafizhdsy@gmail.com>
2025-12-01 01:28:56 +0000

    untuk informasi biasa jangan gunakan warna merah gunakan warna lainnya

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -92,6 +92,7 @@ const ToastClose = React.forwardRef<
       "absolute right-2 top-2 rounded-md p-1 text-foreground/50 opacity-0 transition-opacity hover:text-foreground focus:opacity-100 focus:outline-none focus:ring-2 group-hover:opacity-100",
        "group-[.destructive]:text-red-300 group-[.destructive]:hover:text-red-50 group-[.destructive]:focus:ring-red-400 group-[.destructive]:focus:ring-offset-red-600",
        "group-[.success]:text-green-300 group-[.success]:hover:text-green-50",
+       "ring-primary focus:ring-primary",
       className
     )}
     toast-close=""
